3e16f10b0300935676d035d01b721b270747ac7d
aninda90 <210605110090@student.uin-malang.ac.id>
2023-12-05 19:01:56 +0700

    second

--- pages/data_alternatif/proses_tambah_alternatif.php
+++ pages/data_alternatif/proses_tambah_alternatif.php
@@ -1,64 +1,28 @@
-<!-- Page Heading -->
-<div class="d-sm-flex align-items-center justify-content-between mb-1">
-  <h1 class="h3 text-gray-800">Tambah Alternatif</h1>
-</div>
+<?php
+  session_start();
+	require '../../config/connect_db.php';
 
-<!-- popup status -->
-<?php  
-    if (isset($_SESSION['pesan']) && isset($_SESSION['status'])) :
-?>
-    <div class="alert alert-<?= $_SESSION['status']; ?> rounded-0" role="alert" id="notif">
-      <?= $_SESSION['pesan']; ?>
-    </div>
-<?php  
-    unset($_SESSION['pesan']);
-    unset($_SESSION['status']);
-    endif;
-?>
+	// proses tambah
+  $siswa = htmlspecialchars($_POST['nama_siswa']);
+  $jk = htmlspecialchars($_POST['jenis_kelamin']);
+  $kelas = htmlspecialchars($_POST['kelas']);	
+  $alamat = htmlspecialchars($_POST['alamat']);
 
-<!-- DataTales Example -->
-<div class="card mb-4 rounded-0">
-    <div class="card-header bg-white py-3 d-flex">
-        <h6 class="m-0 text-gray-800">Masukkan Data Alternatif Baru</h6>
-    </div>
-    <div class="card-body">
-        <form action="pages/data_alternatif/proses_tambah_alternatif.php" method="post">
-        
-            <div class="mb-3">
-                <label for="exampleFormControlInput1" class="form-label">Nama Siswa</label>
-                <input type="text" class="form-control rounded-0" id="exampleFormControlInput1" name="nama_siswa" placeholder="Masukkan Nama Siswa"required>
-            </div>
-            <div class="mb-3">
-                <label for="exampleFormControlInput1" class="form-label">JK</label>
-                <select class="form-control rounded-0" aria-label="Default select example" name="jenis_kelamin" required>
-                    <option selected disabled>-- Jenis Kelamin --</option>
-                    <option value="Laki-laki">Laki-laki</option>
-                    <option value="Perempuan">Perempuan</option>
-                </select>
-            </div>
-            <div class="mb-4">
-                <label for="exampleFormControlInput1" class="form-label">Kelas</label>
-                <select class="form-control rounded-0" aria-label="Default select example" name="kelas" required>
-                    <option selected disabled>-- Pilih Kelas --</option>
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                    <option value="5">5</option>
-                    <option value="6">6</option>
-                    
-                </select>
-            </div>
-            <div class="mb-3">
-                <label for="exampleFormControlInput1" class="form-label">Alamat</label>
-                <input type="text" class="form-control rounded-0" id="exampleFormControlInput1" name="alamat" placeholder="Masukkan Alamat Siswa"required>
-            </div>
-            <a href="index.php?page=data_siswa" class="btn btn-secondary btn-square rounded-0">
-                <i class="fas fa-chevron-left fa-sm"></i> Kembali
-            </a>
-            <button type="submit" class="btn btn-success btn-square rounded-0">
-                <i class="fas fa-save fa-sm"></i> Simpan
-            </button>
-        </form>
-    </div>
-</div>
+
+  $sql = "INSERT INTO data_alternatif VALUES ('', '$siswa', '$jk', '$kelas', '$alamat')";
+  $send = mysqli_query($koneksi_db, $sql);
+
+  if ($send) {
+    $_SESSION['pesan'] = 'Alternatif berhasil tersimpan!';
+    $_SESSION['status'] = 'success';
+    header('Location: ../../index.php?page=data_siswa');
+    exit;
+  } 
+  else {
+    $_SESSION['pesan'] = 'Alternatif gagal tersimpan!';
+    $_SESSION['status'] = 'danger';
+    header('Location: ../../index.php?page=data_siswa');
+    exit;
+  }
+  
+?>
--- pages/data_alternatif/tambah_alternatif.php
+++ pages/data_alternatif/tambah_alternatif.php
@@ -35,42 +35,25 @@
                 <label for="exampleFormControlInput1" class="form-label">JK</label>
                 <select class="form-control rounded-0" aria-label="Default select example" name="jenis_kelamin" required>
                     <option selected disabled>-- Jenis Kelamin --</option>
-                    <option value="L">L</option>
-                    <option value="P">P</option>
+                    <option value="Laki-laki">Laki-laki</option>
+                    <option value="Perempuan">Perempuan</option>
                 </select>
             </div>
             <div class="mb-4">
                 <label for="exampleFormControlInput1" class="form-label">Kelas</label>
                 <select class="form-control rounded-0" aria-label="Default select example" name="kelas">
                     <option selected disabled>-- Pilih Kelas --</option>
-                    <option value="X DKV 1">X DKV 1</option>
-                    <option value="X DKV 2">X DKV 2</option>
-                    <option value="X DKV 3">X DKV 3</option>
-                    <option value="XI DKV 1">XI DKV 1</option>
-                    <option value="XI DKV 2">XI DKV 2</option>
-                    <option value="XI DKV 3">XI DKV 3</option>
-                    <option value="XII DKV 1">XII DKV 1</option>
-                    <option value="XII DKV 2">XII DKV 2</option>
-                    <option value="XII DKV 3">XII DKV 3</option>
-                    <option value="X PBKM 1">X PBKM 1</option>
-                    <option value="X PBKM 2">X PBKM 2</option>
-                    <option value="X PBKM 3">X PBKM 3</option>
-                    <option value="XI PBKM 1">XI PBKM 1</option>
-                    <option value="XI PBKM 2">XI PBKM 2</option>
-                    <option value="XI PBKM 3">XI PBKM 3</option>
-                    <option value="XII PBKM 1">XII PBKM 1</option>
-                    <option value="XII PBKM 2">XII PBKM 2</option>
-                    <option value="XII PBKM 3">XII PBKM 3</option>
-                    <option value="X SIJA 1">X SIJA 1</option>
-                    <option value="X SIJA 2">X SIJA 2</option>
-                    <option value="X SIJA 3">X SIJA 3</option>
-                    <option value="XI SIJA 1">XI SIJA 1</option>
-                    <option value="XI SIJA 2">XI SIJA 2</option>
-                    <option value="XI SIJA 3">XI SIJA 3</option>
-                    <option value="XII SIJA 1">XII SIJA 1</option>
-                    <option value="XII SIJA 2">XII SIJA 2</option>
-                    <option value="XII SIJA 3">XII SIJA 3</option>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="2">3</option>
+                    <option value="2">4</option>
+                    <option value="2">5</option>
+                    <option value="2">6</option>
                 </select>
+            </div>
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Alamat</label>
+                <input type="text" class="form-control rounded-0" id="exampleFormControlInput1" name="alamat" placeholder="Masukkan Alamat Siswa"required>
             </div>
             <a href="index.php?page=data_siswa" class="btn btn-secondary btn-square rounded-0">
                 <i class="fas fa-chevron-left fa-sm"></i> Kembali
